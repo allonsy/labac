f53be91bca55c54cd3652e0868b0ccbd4df07054
Alec Snyder <alec.snyder@originate.com>
2015-07-17 17:15:31 -0700

    Add toList functions

--- src/structs.hs
+++ src/structs.hs
@@ -12,10 +12,19 @@ instance Functor PackageTree where
   fmap f (Node val vals) = Node (f val) (map (fmap f) vals)
 
 --listByPredicate: given a package tree, and a predicate, it returns all
---packages that satisfy that predicate in the form of a list 
+--packages that satisfy that predicate in the form of a list
 listByPredicate :: (a -> Bool) -> PackageTree a -> [a]
 listByPredicate p (Node val vals)
   | p val =  val : (vals >>= listByPredicate p)
   | otherwise = vals >>= listByPredicate p
+
+--toListWith: converts a tree to a list tranforming each node with the given
+--function
+toListWith :: (a -> b) -> PackageTree a -> [b]
+toListWith f (Node val vals) = (f val) : (vals >>= toListWith f)
+
+--toList: Converts a tree to a list
+toList :: PackageTree a -> [a]
+toList tr = toListWith id tr
 
 a = Node 1 [Node 2 [], Node 3 [], Node 4 [Node 5 [Node 7 []], Node 6[]]]
